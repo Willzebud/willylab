1a906dea28f9e9eb71d70bfabea3a1361ec7e213
willyam ribiere <willyamrib@gmail.com>
2025-01-20 17:09:06 +0100

    Écran V.1

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -4,12 +4,12 @@ export const Header = () => {
   return (
     <header className="sticky py-4">
       <Section className="flex items-center justify-between">
-        <div className="w-10 h-10 rounded-full border-2 border-black bg-transparent cursor-pointer transition hover:border-primary hover:bg-primary" />
+        <div className="w-10 h-10 rounded-full border border-black bg-transparent cursor-pointer transition hover:border-primary hover:bg-primary" />
         <div className="flex items-center space-x-2">
-          <div className="h-auto w-auto border-2 border-black px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
+          <div className="h-auto w-auto border border-black px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
             <p className="text-black group-hover:text-primary">Tarifs</p>
           </div>
-          <div className="h-auto w-auto border-2 border-black px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
+          <div className="h-auto w-auto border border-black px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
             <p className="text-black group-hover:text-primary">Rendez-vous</p>
           </div>
         </div>
