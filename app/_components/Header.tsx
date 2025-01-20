29b2164aa69f441f6c7ecca7a2b0d58a24386e3c
willyam ribiere <willyamrib@gmail.com>
2025-01-20 17:51:34 +0100

    Écran V.1.1

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -3,13 +3,16 @@ import { Section } from "./Section";
 export const Header = () => {
   return (
     <header className="sticky py-4">
-      <Section className="flex items-center justify-between">
-        <div className="w-10 h-10 rounded-full border border-black bg-transparent cursor-pointer transition hover:border-primary hover:bg-primary" />
-        <div className="flex items-center space-x-2">
-          <div className="h-auto w-auto border border-black px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
+      <Section className="flex items-center justify-between space-between space-between">
+        {/* Cercle à gauche */}
+        <div className="w-8 h-8 md:w-10 md:h-10 rounded-full border border-black bg-transparent cursor-pointer transition hover:border-primary hover:bg-primary" />
+
+        {/* Boutons Tarifs et Rendez-vous */}
+        <div className="flex items-center space-x-2 mr-12 md:mr-0">
+          <div className="h-auto w-auto border border-black px-3 md:px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
             <p className="text-black group-hover:text-primary">Tarifs</p>
           </div>
-          <div className="h-auto w-auto border border-black px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
+          <div className="h-auto w-auto border border-black px-3 md:px-4 py-1 rounded-lg cursor-pointer transition hover:border-primary group">
             <p className="text-black group-hover:text-primary">Rendez-vous</p>
           </div>
         </div>
--- app/_components/Menu.tsx
+++ app/_components/Menu.tsx
@@ -1,14 +1,20 @@
 export const Menu = () => {
   return (
-    <div>
-      <div className="absolute top-1/2 left-4 transform -translate-y-1/2">
-        <div className="w-8 h-auto p-1 mb-2 cursor-pointer transition hover:border-primary group flex flex-col items-center justify-center rounded-lg">
-          <div className="w-1 h-1 m-1 rounded-full bg-black group-hover:bg-primary" />
-          <div className="w-1 h-1 m-1 rounded-full bg-black group-hover:bg-primary" />
-          <div className="w-1 h-1 m-1 rounded-full bg-black group-hover:bg-primary" />
+    <>
+      {/* Barre verticale (Desktop) */}
+      <div className="hidden md:block absolute left-16 top-0 h-full w-[1px] bg-black" />
+
+      {/* Barre horizontale (Mobile) */}
+      <div className="block md:hidden absolute top-16 left-0 w-full h-[1px] bg-black" />
+
+      {/* Menu */}
+      <div className="absolute md:top-1/2 md:left-4 top-4 right-4 transform md:-translate-y-1/2">
+        <div className="w-8 h-8 md:w-10 md:h-10 p-1 mb-2 border border-black md:border md:border-transparent rounded-full cursor-pointer transition md:hover:border-transparent hover:border-primary group flex flex-col items-center justify-center">
+          <div className="w-1 h-1 mb-1 rounded-full bg-black group-hover:bg-primary" />
+          <div className="w-1 h-1 mb-1 rounded-full bg-black group-hover:bg-primary" />
+          <div className="w-1 h-1 rounded-full bg-black group-hover:bg-primary" />
         </div>
       </div>
-      <div className="absolute left-16 top-0 h-full w-[1px] bg-black" />
-    </div>
+    </>
   );
 };
