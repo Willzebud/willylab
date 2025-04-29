6d93a7372bd26cdc0e12a961b490a326b2bdc1e0
willyam ribiere <willyamrib@gmail.com>
2025-04-29 15:34:51 +0200

    V 11.4

--- app/belly-gad/BellySeance.tsx
+++ app/belly-gad/BellySeance.tsx
@@ -103,15 +103,14 @@ export const BellySeance = () => {
           </div>
         </div>
         {/* Image desktop */}
-        <div className="w-full lg:w-1/2 flex justify-center hidden md:flex">
-          <div className="relative w-full max-w-md h-[320px] rounded-xl overflow-hidden shadow-lg">
+        <div className="w-full lg:w-1/2 hidden lg:flex justify-center">
+          <div className="relative w-[300px] h-[300px]">
             <Image
-              src="/images/ImgSoins/2.webp"
-              alt="Image de l'offre : Soin Signature GAD®"
+              src="/images/ImgSi/SI3.webp"
+              alt="Image de l'offre : GAD® Detox Plus 1 séance"
               fill
-              sizes="(max-width: 1024px) 50vw, 600px"
-              className="object-cover rounded-xl"
-              priority
+              sizes="(min-width: 1024px) 300px"
+              className="object-contain"
             />
           </div>
         </div>
