6c7be8ee4e018ee3269078f65a25572bf1c37bf5
willyam ribiere <willyamrib@gmail.com>
2025-05-16 13:10:31 +0200

    V 12.8

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,22 @@ export default function RootLayout({
             }),
           }}
         />
+
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-388768948"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-388768948');
+      `,
+          }}
+        />
       </head>
       <body
         className={cn(
